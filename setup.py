239fcf8bfc0f219f420b86062684569e515a456a
JMankewitz <jmankewitz@berkeley.edu>
2020-09-18 13:29:32 -0700

    reset versioning for pypi release

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="childespy",
-    version="0.3.1",
+    version="0.0.1",
     url="https://github.com/langcog/childespy",
     author="Jessica Mankewitz",
     author_email="jmank@stanford.edu",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=["rpy2>=3.3.5", "numpy>=1.19.2", "pandas==1.1.2"],
+    install_requires=["rpy2>=3.3.5", "numpy>=1.19.2", "pandas>=1.1.2"],
     classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: GNU Affero General Public License v3",
